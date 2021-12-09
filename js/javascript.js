e94877065f22755df20f77bb6c7d2780227b64da
Dima-Zharyy <zharyydimka@gmail.com>
2021-12-09 16:12:13 +0200

    Add new code js
    from book "Грокаем..."

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,27 @@
-alert("Hello World");
+const findSmallest = (arr) => {
+  let smallest = arr[0];
+  let smallestIndex = 0;
+
+  for (i = 0; i < arr.length; i += 1) {
+    if (arr[i] < smallest) {
+      smallest = arr[i];
+      smallestIndex = i;
+    }
+  }
+  return smallestIndex;
+};
+
+const selectionSort = (array) => {
+  let size = array.length;
+  let newArr = [];
+  for (i = 0; i < size; i += 1) {
+    let smallest = findSmallest(array);
+    newArr.push(array[smallest]);
+    array.splice(smallest, 1);
+  }
+  return newArr;
+};
+const numbers = [25, 15, 16, 5, 6, 9, 2, 18, 6, 13];
+
+selectionSort(numbers);
+console.log(selectionSort(numbers));
