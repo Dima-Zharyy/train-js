8a43dba7fd8cba81ea49ecb2a42299247e9a28b9
Dima-Zharyy <zharyydimka@gmail.com>
2021-12-12 12:32:59 +0200

    Update index.html and javacsript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,17 @@
 // alert("Я JavaScript");
 
+/* for (i = 0; i <= 10; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+} */
+
+let i = 0;
+while (i < 3) {
+  console.log(`number ${i}`);
+  i++;
+}
+
 /* let login = prompt("Vvedite login", "");
 let password;
 
