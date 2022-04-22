fd20b04ba5ecf34f46ac0ab069753518a6ae5ca8
dima-zharyy <zharyydimka@gmail.com>
2022-04-22 15:29:14 +0300

    hw-blended-version-1

--- js/hw_blended.js
+++ js/hw_blended.js
@@ -52,33 +52,81 @@ const services = {
       restaurant => restaurant.brand === brand
     );
 
-    return chosenBrand.menu;
+    const menuString = Object.entries(chosenBrand.menu).join(' || ');
+
+    return menuString;
   },
-  addOrder(item, quantity) {},
-  confirmOrder() {},
+  addOrder(item, quantity) {
+    const randomId = Date.now();
+    const findPrice = restaurants.find(
+      restaurant => restaurant.brand === torpedaDelivery.chosenRestaurant
+    ).menu[item];
+    const userOrder = {
+      id: randomId,
+      restaurant: torpedaDelivery.chosenRestaurant,
+      details: {
+        itemOrdered: item,
+        quantityOrdered: quantity,
+        price: findPrice,
+      },
+    };
+    torpedaDelivery.order.push(userOrder);
+  },
+  confirmOrder() {
+    const sum = torpedaDelivery.order.reduce(
+      (acc, item) => (acc = item.details.quantityOrdered * item.details.price),
+      0
+    );
+    const deliveryTime = restaurants.find(
+      restaurant => restaurant.brand === torpedaDelivery.chosenRestaurant
+    ).deliveryTime;
+    const finishOrder = confirm(
+      'Please confirm your order! If you want to add more in your order, please enter Отмена'
+    );
+    if (finishOrder) {
+      alert(`Total sum of your order is: ${sum}
+      Delivery time: ${deliveryTime} `);
+
+      const userRest = restaurants.find(
+        restaurant => restaurant.brand === torpedaDelivery.chosenRestaurant
+      ).order;
+      const orderToCopy = [...torpedaDelivery.order];
+      userRest.push(orderToCopy);
+      torpedaDelivery.order = [];
+      torpedaDelivery.chosenRestaurant = '';
+      return;
+    }
+    alert('Ok you can choose another restaurant');
+    torpedaDelivery.chooseRestaurant();
+  },
 };
 
 const torpedaDelivery = {
   order: [],
   chosenRestaurant: '',
-  getAvailableRestaurants() {
+  getAvailableRestaurantsList() {
     const restaurantsList = restaurants.map(({ brand }) => `${brand}`);
     const restaurantsListToString = restaurantsList.join(' || ');
-    alert(`Available restaurants: ${restaurantsListToString}`);
+    // alert(`Available restaurants: ${restaurantsListToString}`);
+    return restaurantsListToString;
+  },
+
+  getAvailableRestaurantsData() {
+    const restaurantsList = restaurants.map(({ brand }) => brand);
     return restaurantsList;
   },
+
   chooseRestaurant() {
-    const restList = this.getAvailableRestaurants();
-    const restListNormalized = restList.map(rest => rest.toLowerCase().trim());
+    const restList = this.getAvailableRestaurantsData();
+    const restListNormalized = restList.map(rest => rest.toLowerCase());
     const selectedBrand = prompt(
-      `Please choose a restaurant from the list: ${restList}`
-    ).trim();
+      `Please choose a restaurant from the list:
+       ${this.getAvailableRestaurantsList()}`
+    );
     if (selectedBrand === null) {
       alert('See you next time!');
       return;
     }
-
-    // console.log(restListNormalized);
 
     if (!restListNormalized.includes(selectedBrand.toLowerCase())) {
       alert(
@@ -92,11 +140,64 @@ const torpedaDelivery = {
     );
 
     this.chosenRestaurant = restList[indexOfChosenRest];
-    // console.log(this.chosenRestaurant);
+    alert(`Ok! You chose ${this.chosenRestaurant}`);
   },
-  chooseDishes() {},
+
+  chooseDishes() {
+    const menu = services.getMenu(this.chosenRestaurant);
+    alert(`Here is avaliable positions in menu: ${menu}`);
+
+    const positionChoice = prompt('Please type what do you want to order');
+
+    if (positionChoice === null) {
+      alert('Have a good day!');
+      return;
+    }
+
+    const positions = restaurants.find(
+      restaurant => restaurant.brand === this.chosenRestaurant
+    ).menu;
+    const keys = Object.keys(positions).map(key => key);
+    if (!keys.includes(positionChoice)) {
+      alert(
+        `There is no item in menu with name ${positionChoice}! Try again please!`
+      );
+      return this.chooseDishes();
+    }
+
+    const quantity = Number(
+      prompt('Please type how mmany do you want to order')
+    );
+
+    if (quantity === null) {
+      alert('You entered nothing! Please try again!');
+      return this.chooseDishes();
+    }
+
+    if (isNaN(quantity)) {
+      alert('Please enter only number! Try again');
+      return this.chooseDishes();
+    }
+
+    services.addOrder(positionChoice, quantity);
+    services.confirmOrder();
+  },
 };
 
-// torpedaDelivery.chooseRestaurant();
-// torpedaDelivery.getAvailableRestaurants();
-// torpedaDelivery.chooseRestaurant();
+torpedaDelivery.chooseRestaurant();
+torpedaDelivery.chooseDishes();
+
+console.log(torpedaDelivery);
+console.log(restaurants);
+
+torpedaDelivery.chooseRestaurant();
+torpedaDelivery.chooseDishes();
+
+console.log(torpedaDelivery);
+console.log(restaurants);
+
+torpedaDelivery.chooseRestaurant();
+torpedaDelivery.chooseDishes();
+
+console.log(torpedaDelivery);
+console.log(restaurants);
